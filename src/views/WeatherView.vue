8b411cbfdd5c86f58059aa47938bf5f49593be62
jokejee <jokejee29@gmail.com>
2023-10-10 01:28:44 +0700

    first commit

---
+++ src/views/WeatherView.vue
@@ -0,0 +1,55 @@
+<script setup>
+import axios from 'axios'
+import { ref, onMounted } from 'vue';
+import { RouterLink } from 'vue-router'
+import { Travel_listStore } from '../stores/counter.js';
+
+const All_travel = Travel_listStore();
+const All_travel_list = ref(All_travel.travel_list);
+
+
+
+const items = ref([])
+const url = ref('{{i.ip}}')
+
+function fetchPosts() {
+  axios
+    .get(url.value)
+    .then((response) => {
+      items.value = response.data
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+}
+
+onMounted(fetchPosts)
+</script>
+
+<template>
+  <div class="row row-cols-1 row-cols-md-3 g-4">
+    <div class="col" v-for="(i,index) in All_travel_list " :key="index">
+      <div class="card h-100" style="background-color: rgb(90, 163, 139);">
+        <img :src="i.img" class="card-img-top" alt="...">
+        <div class="card-body" style="background-color: rgb(109, 183, 207);">
+          <h5 class="card-title"><strong>{{i.name}}</strong></h5>
+          <p class="card-text">CHECK WATHER</p>
+        </div>
+        <div class="card-footer">
+          <RouterLink :to="`/WeatherCard/${index}`">
+            <button class="btn btn-primary" type="submit" style="background-color: black;">CHECK</button>
+          </RouterLink>
+          
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.card-img-top {
+    width: 100%;
+    max-height: 400px;
+}
+
+</style>
